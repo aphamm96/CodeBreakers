2b4d87ea4dd257fcd79878df3f2da21d2ba8ddd0
aphamm96 <aphamm96@gmail.com>
2020-06-01 10:18:49 -0400

    Added reference

--- CodeBreakersAPI/App_Start/IdentityConfig.cs
+++ CodeBreakersAPI/App_Start/IdentityConfig.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using CodeBreakersAPI.Models;
+using CodeBreakers.Data;
 
 namespace CodeBreakersAPI
 {
