e25d330f52dfaa389f7b24fd9445c1d22d38ea89
aphamm96 <aphamm96@gmail.com>
2020-06-01 11:03:27 -0400

    User Class added and other things in identitymodels

---
+++ CodeBreakers.Data/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBreakers.Data
+{
+    public class User
+    {
+        [Key]
+        public Guid UserId { get; set; }
+        [Required(ErrorMessage = "User Name is required")]
+        [Display(Name = "User Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+    }
+}
