bb54b8eb156284e211201fe88f41c65e05708bc1
Sabra Snider <sabralsnider@gmail.com>
2020-06-01 10:43:48 -0400

    Added Comment and Reply classes

---
+++ CodeBreakers.Data/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBreakers.Data
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Text { get; set; }
+        [ForeignKey("Author")]
+        [Required]
+        public Guid UserId { get; set; }
+        public virtual User Author { get; set; }
+    }
+}
